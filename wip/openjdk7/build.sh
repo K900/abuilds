6548f74b1cd724db8fd3a608bec8322d04960560
K900 <k0009000@gmail.com>
2011-12-30 14:01:29 +0400

    Prepare OpenJDK7 as Oracle Java replacement, update and rename KDE NM stuff.

--- wip/openjdk7/build.sh
+++ wip/openjdk7/build.sh
@@ -11,13 +11,13 @@ echo "=== You need sudo access!"
 rm -rf ~/openjdk7-packages/
 mkdir -p ~/openjdk7-packages/
 
+build_dep run-parts
 build_dep ca-certificates
 build_dep ca-certificates-java
 build_dep eclipse-ecj
 build_dep giflib
 build_dep lcms2
 build_dep rhino
-build_dep run-parts
 build_dep xalan-java
 build_dep xerces2-java
 build_dep zlib
